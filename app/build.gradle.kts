1d8d909546cb7fdb4c3f3ea1bd869c3205a7315d
Oliver Wirthgen <oliver.wirthgen@gmail.com>
2026-03-23 14:05:32 +0100

    Bump versionCode to 3, versionName to 3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.example.voicelauncher"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "3.0"
 
         buildConfigField("String", "GEMINI_API_KEY", "\"$geminiApiKey\"")
     }
